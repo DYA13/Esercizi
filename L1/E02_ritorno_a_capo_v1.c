abac235bb5e470f29e6c732f063adc383684191c
Yelena Japarova <yelena.dzhaparova@gmail.com>
2026-03-04 16:00:55 +0100

    solveв exercises E02 and E03

---
+++ L1/E02_ritorno_a_capo_v1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("Ciao\n ");
+    printf("Mondo\n");
+
+    return 0;
+};
---
+++ L1/E03_tab_colonne.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    printf("Lun\t\t10.30\t\tProgrammazione 1\n");
+    printf("Mar\t\t14.00\t\tAlgebra\n");
+    printf("Mer\t\t8.30\t\tGeometria\n");
+
+    return 0;
+};
