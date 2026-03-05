8024baa0cd6c7fdbb9c5340d0ff422b4342b9416
Yelena Japarova <yelena.dzhaparova@gmail.com>
2026-03-05 10:35:50 +0100

    completed  L1/E06_range_reali.c

---
+++ L1/E05_range_integrali.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<limits.h>
+
+
+int main(){
+
+    printf("CHAR_MIN = %d\n",CHAR_MIN); // char = 8 bit
+    printf("CHAR_MAX = %d\n",CHAR_MAX);
+    printf("INT_MIN = %d\n",INT_MIN);// int = 32 bit
+    printf("INT_MAX = %d\n",INT_MAX);
+    return 0;
+};
---
+++ L1/E06_range_reali.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<float.h>
+
+int main(){
+
+    printf("FLT_MAX = %g\n", FLT_MAX); //FLT_MAX = 3.40282e+38
+    printf("FLT_MIN = %g\n",FLT_MIN);// FLT_MIN = 1.17549e-38
+
+    printf("DL_MAX = %g\n",DBL_MAX);//DL_MAX = 1.79769e+308
+    printf("DBL_MIN = %g\n",DBL_MIN);//DBL_MIN = 2.22507e-308
+
+    return 0;
+};
