63947a24a9088e319bc925171e17c9bff402d20c
Yelena Japarova <yelena.dzhaparova@gmail.com>
2026-03-05 09:43:50 +0100

    completed L1/E04_ascii_hex.c, L1/E04_ascii_octal.c

---
+++ L1/E04_ascii_hex.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("\x43\x69\x61\x6F\n");
+
+    return 0;
+};
---
+++ L1/E04_ascii_octal.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("\103\151\141\157\n");
+    
+    return 0;
+};
