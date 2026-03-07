1b053b9354f1a9579ec6303d63b724737336eaee
Yelena Japarova <yelena.dzhaparova@gmail.com>
2026-03-07 12:24:16 +0100

    Aggiunto controllo input per Esercizio 3, L2

---
+++ L2/E03_euclide_validazione.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    printf("Welcome to Euclide Calculator!\n");
+
+    printf("Inserisci il primo numero intero:\n");
+    scanf("%d", &a);
+    printf("Inserisci il secondo numero intero:\n");
+    scanf("%d", &b);
+
+    if (a <= 0 || b <= 0)
+    {
+        printf("Errore: a e b devono essere maggiori di 0\n");
+        return -1;
+    };
+
+    int x = a;
+    int y = b;
+
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
+        };
+    };
+    printf("Il mcd del %d e %d e': %d\n", x, y, a);
+
+    return 0;
+};
